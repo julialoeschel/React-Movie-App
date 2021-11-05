4dfc842a4581bd1dc599e6bf0d18384f425b7b3d
Julian Buchholz <github@julianbuchholz.com>
2021-11-05 15:01:06 +0100

    Add checkbox watched

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,11 +9,13 @@ function App(): JSX.Element {
       movieTitle: 'James Bond - Keine Zeit zu sterben',
       movieDescription:
         'James Bond ist nicht mehr als Geheimagent im Dienst und genießt seinen Ruhestand auf Jamaika. Doch seine Atempause ist nur von kurzer Dauer, denn der CIA-Agent Felix Leiter spürt Bond auf, um ihn um Hilfe zu bitten.',
+      movieWatched: false,
     },
     {
       movieTitle: 'Dune',
       movieDescription:
         'Feature adaptation of Frank Herbert’s science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy.',
+      movieWatched: true,
     },
   ]);
 
@@ -24,6 +26,7 @@ function App(): JSX.Element {
         <Cards
           movieTitle={movie.movieTitle}
           movieDescription={movie.movieDescription}
+          movieWatched={movie.movieWatched}
         />
       ))}
     </>
--- src/app/components/Cards/Cards.tsx
+++ src/app/components/Cards/Cards.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './Cards.module.css';
+import Checkbox from '../Checkbox/Checkbox';
 
 type CardsProps = {
   movieTitle: string;
   movieDescription: string;
+  movieWatched: boolean;
 };
 
-function Cards({ movieTitle, movieDescription }: CardsProps): JSX.Element {
+function Cards({
+  movieTitle,
+  movieDescription,
+  movieWatched,
+}: CardsProps): JSX.Element {
   return (
     <li className={styles.cards}>
       <h2>{movieTitle}</h2>
       <p>{movieDescription}</p>
+      <Checkbox movieWatched={movieWatched} />
     </li>
   );
 }
