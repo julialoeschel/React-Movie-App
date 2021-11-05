aa5c2693ae3e0661d562e7801352c9f47f920d3f
Julia Loeschel <juloeschel@gmail.com>
2021-11-05 12:23:59 +0100

    Setup header and Cards
    Co-authored-by: Julian Buchholz <derjubu@users.noreply.github.com>

---
+++ src/app/components/Title/Title.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Title.module.css';
+
+function Title(): JSX.Element {
+  return <h1 className={styles.title}>Movies to watch</h1>;
+}
+
+export default Title;
